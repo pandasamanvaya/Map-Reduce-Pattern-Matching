4815de2f9679c395a45a8995c14771da0ab648e1
pandasamanvaya <pandasamanvaya@gmail.com>
2021-01-01 21:46:11 +0530

    Modified scripts

--- run_naive.sh
+++ run_naive.sh
@@ -2,5 +2,8 @@
 # python gen_input.py
 $HADOOP_HOME/bin/hadoop com.sun.tools.javac.Main MRNaiveSearch.java 
 jar cf mrnaive.jar MRNaiveSearch*.class
-rm -r output
-time $HADOOP_HOME/bin/hadoop jar mrnaive.jar MRNaiveSearch sample_100000.txt output key_100000.txt
+if [ -d "./output" ]
+then
+	rm -r output
+fi
+time $HADOOP_HOME/bin/hadoop jar mrnaive.jar MRNaiveSearch sample_temp.txt output key_temp.txt
--- run_qs.sh
+++ run_qs.sh
@@ -2,5 +2,8 @@
 # python gen_input.py
 $HADOOP_HOME/bin/hadoop com.sun.tools.javac.Main MRQuickSearch.java 
 jar cf mrquick.jar MRQuickSearch*.class
-rm -r output
-time $HADOOP_HOME/bin/hadoop jar mrquick.jar MRQuickSearch sample_100000.txt output key_100000.txt
+if [ -d "./output" ]
+then
+	rm -r output
+fi
+time $HADOOP_HOME/bin/hadoop jar mrquick.jar MRQuickSearch sample_temp.txt output key_temp.txt
